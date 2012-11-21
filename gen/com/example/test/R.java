81a7d8e9bfc5aa605de14fcdaf335daa9433dd9b
backwang <backwang@qq.com>
2012-11-21 14:47:25 +0800

    这是第六次提交

--- gen/com/example/test/R.java
+++ gen/com/example/test/R.java
@@ -14,9 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int tableRow1=0x7f070001;
-        public static final int widget0=0x7f070000;
-        public static final int widget32=0x7f070002;
+        public static final int ivLogo=0x7f070000;
+        public static final int ratingbar=0x7f070005;
+        public static final int rl_name=0x7f070001;
+        public static final int rl_score=0x7f070003;
+        public static final int tvApplicationName=0x7f070002;
+        public static final int tvRating=0x7f070004;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;

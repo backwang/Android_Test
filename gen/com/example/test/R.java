a37e900db61fd11d2547260dda37e140052bad7a
backwang <backwang@qq.com>
2012-12-02 10:52:32 +0800

    去掉了星条

--- gen/com/example/test/R.java
+++ gen/com/example/test/R.java
@@ -17,9 +17,7 @@ public final class R {
         public static final int button1=0x7f070001;
         public static final int button2=0x7f070003;
         public static final int ivLogo=0x7f070000;
-        public static final int ratingbar=0x7f070005;
         public static final int tvApplicationName=0x7f070002;
-        public static final int tvRating=0x7f070004;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;

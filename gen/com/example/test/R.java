fb129ea281924856274e5658c533c9cab5866a3e
backwang <backwang@qq.com>
2012-12-03 17:04:52 +0800

    自定义适配器的使用

--- gen/com/example/test/R.java
+++ gen/com/example/test/R.java
@@ -14,10 +14,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070003;
-        public static final int ivLogo=0x7f070000;
-        public static final int tvApplicationName=0x7f070002;
+        public static final int bigtv=0x7f070001;
+        public static final int btn=0x7f070003;
+        public static final int cb=0x7f070004;
+        public static final int iv=0x7f070000;
+        public static final int smalltv=0x7f070002;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
--- src/com/example/test/LoginActivity.java
+++ src/com/example/test/LoginActivity.java
@@ -2,6 +2,8 @@ package com.example.test;
 
  
  
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -16,54 +18,47 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Vibrator;
-import android.telephony.gsm.SmsManager;
+ 
 import android.view.Gravity;
 import android.view.View;
+ 
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
  
  
  
 public class LoginActivity extends Activity {
 	 
-	public static final String EXTRA_EMAIL = "com.example.android.authenticatordemo.extra.EMAIL";
-
-	/**
-	 * Keep track of the login task to ensure we can cancel it if requested.
-	 */
 	 
-
-	//发送短消息
-	public void sendsms(View view)
-	{
+		private SimpleAdapter adapter;// 声明适配器对象 
+		private ListView listView; // 声明列表视图对象 
+		private List<Map<String, Object>> list;// 声明列表容器 
+		public static LoginActivity ma; 
+		@Override 
+		public void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState); 
+		ma = this; 
+		// 实例化列表容器 
+		list = new ArrayList<Map<String, Object>>(); 
+		listView = new ListView(this);// 实例化列表视图 
+		// 实例一个列表数据容器 
+		Map<String, Object> map = new HashMap<String, Object>(); 
+		// 往列表容器中添加数据 
+		map.put("item1_imageivew", R.drawable.ic_launcher); 
+		map.put("item1_bigtv", "BIGTV"); 
+		map.put("item1_smalltv", "SMALLTV"); 
+		// 将列表数据添加到列表容器中 
+		list.add(map); 
+		// --使用系统适配器，无法实现组件监听； 
+		// //实例适配器 
+		adapter = new SimpleAdapter(this, list, R.layout.activity_login, new String[] { 
+		"item1_imageivew", "item1_bigtv", "item1_smalltv" }, new int[] { 
+		R.id.iv, R.id.bigtv, R.id.smalltv }); 
+		listView.setAdapter(adapter); 
+		// //显示列表视图 
+		this.setContentView(listView); 
+		} 
 		 
-		 
- 
- 
-	 IntentFilter intentFilter=new IntentFilter();
-	 intentFilter.addAction("ACTION_TIME_CHANGED");
- 
- 
-	 IntentFilter intentFilter1=new IntentFilter();
-	 intentFilter1.addAction("ACTION_TIME_CHANGED");
-	 registerReceiver(null, intentFilter1);
- 
-	}
-	public void sendmsg(View view)
-	{
-		Toast toast=new Toast(this);
-		toast = Toast.makeText(getApplicationContext(), 
-				"我发了一条消息", Toast.LENGTH_LONG); 
-				toast.setGravity(Gravity.CENTER, 0, 0); 
-				toast.show(); 
-	}
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.activity_login);
-
-		// Set up the login form.
-	 
-	}
 }

6069766c2caef02467cb9228fb49583b3f5346df
backwang <backwang@qq.com>
2013-01-04 11:15:54 +0800

    没有发动

---
+++ src/com/example/test/Myclass.java
@@ -0,0 +1,17 @@
+package com.example.test;
+
+import java.io.Serializable;
+
+public class Myclass implements Serializable{
+
+	private String nameString="bk";
+	public String ageString="20";
+	public String getNameString() {
+		return nameString;
+	}
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	
+	
+}
